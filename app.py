e5be61dbca65b40953d961024bf896511d5a56f7
TRABAJO <TRABAJO@MacBook-Air-de-Idalia.local>
2025-03-02 16:04:01 -0600

    gráficos, casillas de verificación y botones

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ car_data = pd.read_csv('https://practicum-content.s3.us-west-1.amazonaws.com/new
 fig = px.histogram(car_data, x='odometer', y='price') #crear histograma
 fig.show() #mostrar histograma
 
-fig = px.scatter(car_data, x='odometer', y='price') #crear scatter plot
-fig.show() #mostrar scatter plot
+fig_2 = px.scatter(car_data, x='odometer', y='price') #crear scatter plot
+fig_2.show() #mostrar scatter plot
 
 hist_button = st.button('Construir histograma') #botón para mostrar histograma
 if hist_button: #si se presiona el botón
@@ -19,12 +19,12 @@ if hist_button: #si se presiona el botón
     fig = px.histogram(car_data, x='odometer') #crear histograma
     st.plotly_chart(fig, use_contained_width=True) #mostrar histograma
 
-scatter_button = st.button('Construir histograma') #botón para mostrar histograma
+scatter_button = st.button('Construir diagrama de dispersión') #botón para mostrar diagrama de dispersión
 if scatter_button: #si se presiona el botón
     #escribir un mensaje
     st.write('Creación de un diagrama de dispersión para el conjunto de datos de anuncios de venta de coches')
-    fig = px.scatter(car_data, x='odometer') #crear histograma
-    st.plotly_chart(fig, use_contained_width=True) #mostrar histograma    
+    fig_2 = px.scatter(car_data, x='odometer') #crear histograma
+    st.plotly_chart(fig_2, use_contained_width=True) #mostrar histograma    
 
 #crear casillas de verificación
 build_histogram = st.checkbox('Construir un histograma') #casilla de verificación
